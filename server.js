d0968d7ff5b75927c5e6e0c56f2d72a62746e22b
dietamake <dietamake33@gmail.com>
2026-03-18 22:28:27 +0100

    server.js

--- server.js
+++ server.js
@@ -179,7 +179,3 @@ const PORT = process.env.PORT || 3000
 app.listen(PORT, () => {
   console.log(`Servidor corriendo en puerto ${PORT}`)
 })
-
-app.listen(PORT, () => {
-  console.log(`Servidor corriendo en puerto ${PORT}`)
-})
